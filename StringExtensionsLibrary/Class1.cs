289267e01cc0604643a2b7c31297a94972f5c599
Ripudaman Singh <rsin1191@my.centennialcollege.ca>
2026-04-02 20:05:16 +0530

    Add project files.

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+}
